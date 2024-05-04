12fac3c165e8be1e17b75b8afb7dfbcb2ec2923c
Kapi <zuzannaraszka@gmail.com>
2024-05-04 19:52:50 +0200

    Rep

---
+++ Main.java
@@ -0,0 +1,36 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
+
+
+public class Main {
+  public static void main(String[] args) {
+    //Zad 4
+    final int b = 10;
+    Person[] people = new Person[5];
+
+
+    int result = MathUtils.add(2, 6);
+
+    //Zad 1
+    try {
+
+      Person person1 = new Person("Andrzej - zad1", 89);
+
+      System.out.println(person1.getName() + " ma " + person1.getAge() + " lat.");
+    
+    } 
+    catch (InvalidAgeException e) {
+
+      System.out.println(e.getMessage());
+    }
+
+    //Zad 2 + koniec zadania 3
+    EmailMessenger emailMessenger = new EmailMessenger();
+    emailMessenger.sendMessage("To jest wiadomość Email z wynikiem dodawania :)");
+
+  }
+    
+  }
+}
