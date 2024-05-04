75d63a148419eb1cbfcde041728992bf6aee0661
Raszka1 <166324918+Raszka1@users.noreply.github.com>
2024-05-04 20:17:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,36 +8,11 @@ public class Main {
   public static void main(String[] args) {
     //Zad 4
     final int b = 10;
-    Person[] people = new Person[5];
+    
 
 
     int result = MathUtils.add(2, 6);
-
-
-    try {
-
-      people[0] = new Person("IronMan", 30);
-      people[0] = new Person("BlackWidow", 21);
-      people[0] = new Person("Hulk", 55);
-      people[0] = new Person("CapitanAmeryka", 48);
-      people[0] = new Person("Jarvis", 62);
-
-      Person person1 = new Person("Andrzej - zad1", 89);
-
-      System.out.println(person1.getName() + " ma " + person1.getAge() + " lat.");
-    
-    } 
-    catch (InvalidAgeException e) {
-
-      System.out.println(e.getMessage());
-    }
-
-    //Zad 2 + koniec zadania 3
-    EmailMessenger emailMessenger = new EmailMessenger();
-    emailMessenger.sendMessage("To jest wiadomość Email z wynikiem dodawania :)"+ result);
-
-
-     //zad 4 - koniec
+     //zad 4 
     for (int i = 0; i < people.length; i++) {
       if (people[i] != null) {
           int value = MathUtils.add(people[i].getAge(), b);
