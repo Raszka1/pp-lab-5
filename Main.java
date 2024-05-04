6f903b6acc2d1e2f1a8b48c9f43156e1a1c253c8
Kapi <zuzannaraszka@gmail.com>
2024-05-04 18:52:13 +0200

    Zad4

--- Main.java
+++ Main.java
@@ -1,14 +1,32 @@
+//Zad1
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 //zad2
-import company.implementations.EmailMessenger;;
+import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
+
 
 public class Main {
   public static void main(String[] args) {
+    //Zad 4
+    final int b = 10;
+    Person[] people = new Person[5];
+
+
+    int result = MathUtils.add(2, 6);
 
     //Zad 1
     try {
-      Person person1 = new Person("Andrzej", 89);
+      //Zad4
+
+      people[0] = new Person("IronMan", 30);
+      people[0] = new Person("BlackWidow", 21);
+      people[0] = new Person("Hulk", 55);
+      people[0] = new Person("CapitanAmeryka", 48);
+      people[0] = new Person("Jarvis", 62);
+
+
+      Person person1 = new Person("Andrzej - zad1", 89);
 
       System.out.println(person1.getName() + " ma " + person1.getAge() + " lat.");
     } 
@@ -18,7 +36,7 @@ public class Main {
     }
 
     try {
-      Person person2 = new Person("Kuba", 133);
+      Person person2 = new Person("Kuba - zad1", 133);
 
       System.out.println(person2.getName() + " ma " + person2.getAge() + " lat.");
     } 
@@ -27,8 +45,18 @@ public class Main {
       System.out.println(e.getMessage());
     }
 
-    //Zad 2
+    //Zad 2 + koniec zadania 3
     EmailMessenger emailMessenger = new EmailMessenger();
-    emailMessenger.sendMessage("To jest wiadomość Email :)");
+    emailMessenger.sendMessage("To jest wiadomość Email z wynikiem dodawania:" + result);
+
+    //zad 4 - koniec
+    for (int i = 0; i < people.length; i++) {
+      if (people[i] != null) {
+          int value = MathUtils.add(people[i].getAge(), b);
+          // Wykorzystaj wartość w treści wiadomości metody sendMessage
+          emailMessenger.sendMessage(people[i].getName() + "'s wartosc to: " + value);
+      }
+  }
+    
   }
 }
