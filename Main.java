2a8b6167aa9f24265c0b27516ef154f55f3ad4fb
Kapi <zuzannaraszka@gmail.com>
2024-05-04 19:15:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,4 @@
 //Zad1
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
-//zad2
-import company.implementations.EmailMessenger;
-import company.utils.MathUtils;
 
 
 public class Main {
@@ -26,19 +21,7 @@ public class Main {
       people[0] = new Person("Jarvis", 62);
 
 
-      Person person1 = new Person("Andrzej - zad1", 89);
-
-      System.out.println(person1.getName() + " ma " + person1.getAge() + " lat.");
-    } 
-    catch (InvalidAgeException e) {
-
-      System.out.println(e.getMessage());
-    }
-
-    try {
-      Person person2 = new Person("Kuba - zad1", 133);
-
-      System.out.println(person2.getName() + " ma " + person2.getAge() + " lat.");
+    
     } 
     catch (InvalidAgeException e) {
 
